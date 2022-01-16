c3fd4f823daa955a08c916a320779fe23768b969
GSP <gowri.shankar@tecnotree.com>
2022-01-16 10:50:16 +0530

    Latest changes

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,26 +1,72 @@
-import React from 'react'
-import {useFormik} from 'formik';
+import React from "react";
+import { useFormik } from "formik";
 
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
 
+const onSubmit = (values) => {
+  console.log("values", values);
+};
+
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = "required";
+  }
+  if (!values.email) {
+    errors.email = "required";
+  }
+  if (!values.channel) {
+    errors.channel = "required";
+  }
+  return errors;
+};
 function YoutubeForm() {
-    const formik = useFormik({})
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
 
-    return (
-        <div>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input type='text' id='name' name='name' />
-                
-                <label htmlFor="email">E-mail</label>
-                <input type='text' id='email' name='email' />
+  console.log("form", formik);
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
-                <label htmlFor="channel">Channel</label>
-                <input type='text' id='channel' name='channel' />
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default YoutubeForm
+export default YoutubeForm;
